173ac2644d860bd480b8b41ed4816ed7b32b89db
abdelazizhanafi <abdelazizhaanafy@gmail.com>
2025-02-17 14:55:15 +0200

    -change map style

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -11,43 +11,31 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
   late GoogleMapController googleMapController;
+  String? nightMapStyle;
   @override
   void initState() {
     initialCameraPosition = CameraPosition(
         zoom: 13, target: LatLng(26.559611633104257, 31.69568211049024));
+    initMapStyle();
     super.initState();
   }
 
   @override
-  void dispose() {
-    googleMapController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      style: nightMapStyle,
+      onMapCreated: (controller) {
+        googleMapController = controller;
+        initMapStyle();
+      },
+      initialCameraPosition: initialCameraPosition,
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GoogleMap(
-            onMapCreated: (controller) {
-              googleMapController = controller;
-            },
-            // cameraTargetBounds: CameraTargetBounds(LatLngBounds(
-            //     southwest: LatLng(26.529136478194715, 31.643660399853797),
-            //     northeast: LatLng(26.579808890303237, 31.723740428642913))),
-            initialCameraPosition: initialCameraPosition),
-        Positioned(
-            bottom: 16,
-            right: 16,
-            left: 16,
-            child: ElevatedButton(
-                onPressed: () {
-                  googleMapController.animateCamera(CameraUpdate.newLatLng(
-                      LatLng(26.70761354001327, 31.602402588558462)));
-                },
-                child: Text('Change Location')))
-      ],
-    );
+  void initMapStyle() async {
+    nightMapStyle = await DefaultAssetBundle.of(context)
+        .loadString('assets/map_styles/night_map_style.json');
+    setState(() {});
   }
 }
 
